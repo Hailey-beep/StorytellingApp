d51d7221641cb1978df2a42355dbb30da20795d6
Hailey <72172315+Hailey-beep@users.noreply.github.com>
2022-05-17 00:29:49 -0400

    added files

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB8x9NbLTkSWANGHytPkRsczrepTzxIMm4",
+    authDomain: "storytelling-2bcc2.firebaseapp.com",
+    projectId: "storytelling-2bcc2",
+    storageBucket: "storytelling-2bcc2.appspot.com",
+    messagingSenderId: "716486969363",
+    appId: "1:716486969363:web:e4ed2604924d7a2e6b17e6"
+  };
+  
